0ed465897c1204f177a3ded75965aba0c00ee89c
Rikoairlan <rikoairlan57@gmail.com>
2022-06-06 19:12:56 +0700

    create footer

--- index.php
+++ index.php
@@ -156,14 +156,75 @@ session_start();
         
        
         <!-- FOOTER SECTION ----------------------- -->
-        <section class="popular">
-            <div class="container">
-                <div class="title text-xs-center m-b-30" >
-                    <h2 class= "title">Mitras</h2>
-                    
+        <section class="footerSection">
+            <div class="contentContainer container">
+                <div class="footerIntro">
+                    <div class="footerLogoDiv">
+                        <span class="hotelName">
+                            Angkring<span>..</span>
+                        </span>
+                    </div>
+                    <p>Angkring adalah sebuah website yang menawarkan pengiriman makanan via online di restoran favorite mu.</p>
+
+                    <div class="footContactDetails">
+                        <div class="info">
+                            <div class="iconDiv"><i class='bx bx-mail-send' ></i></div>
+                            <span>Angkring@gmail.com</span>
+                        </div>
+
+                        <div class="info">
+                            <div class="iconDiv"><i class='bx bxs-phone-outgoing'></i></div>
+                            <span>+62 8990761528</span>
+                        </div>
+
+                        <div class="info">
+                            <div class="iconDiv"><i class='bx bx-current-location' ></i></div>
+                            <span>jl.blok duku, jakarta timur</span>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="linksDiv">
+                    <div class="footersectionTitle">
+                        <h5>Mitra</h5>
+                    </div>
+                    <ul>
+                        <span>Gojek</span>
+                        <span>Grab</span>
+                        <span>Paypal</span>
+                        <span>Affiliation</span>
+                        <span>FAQs</span>
+                    </ul>
+                </div>
+
+                <div class="linksDiv">
+                    <div class="footersectionTitle">
+                        <h5>OUR SERVICES</h5>
+                    </div>
+                    <ul>
+                        <span>Online Order Food</span>
+                        <span>Free Ongkir</span>
+                        <span>Cash on Delivery</span>
+                        <span>Wishlist</span>
+                        <span>Discount</span>
+                    </ul>
+                </div>
+
+                <div class="linksDiv footerForm">
+                    <div class="footersectionTitle">
+                        <h5> OUR NEWSLETTER</h5>
+                    </div>
+
+                    <form action="">
+                        <label> Subscribe To Our Newsletter...</label>
+                        <input type="text" placeholder="Name" required>
+                        <input type="email" placeholder="Email" required>
+                        <button type="submit">Submit</button>
+                    </form>
                 </div>
             </div>
         </section>
+        <!-- FOOTER SECTION END----------------- -->
     
     <!-- Bootstrap core JavaScript
     ================================================== -->
