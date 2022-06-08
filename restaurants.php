569c0d2105f526aa6d5c6c55d2b21e93fd6535fd
Rikoairlan <rikoairlan57@gmail.com>
2022-06-08 20:32:04 +0700

    merapikan

--- restaurants.php
+++ restaurants.php
@@ -13,7 +13,7 @@ session_start();
     <meta name="description" content="">
     <meta name="author" content="">
     <link rel="icon" href="#">
-    <title>Angkring</title>
+    <title>Angkringan</title>
     <!-- Bootstrap core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/font-awesome.min.css" rel="stylesheet">
@@ -31,7 +31,7 @@ session_start();
         <nav class="navbar navbar-dark">
             <div class="container">
                 <button class="navbar-toggler hidden-lg-up" type="button" data-toggle="collapse" data-target="#mainNavbarCollapse">&#9776;</button>
-                <a class="navbar-brand" href="index.php"> Angkring</a>
+                <a class="navbar-brand" href="index.php"> Angkringan</a>
                 <div class="collapse navbar-toggleable-md  float-lg-right" id="mainNavbarCollapse">
                     <ul class="nav navbar-nav">
                         <li class="nav-item"> <a class="nav-link active" href="index.php">Home <span class="sr-only">(current)</span></a> </li>
@@ -151,15 +151,15 @@ session_start();
                 <div class="footerIntro">
                     <div class="footerLogoDiv">
                         <span class="hotelName">
-                            Angkring
+                            Angkringan
                         </span>
                     </div>
-                    <p>Angkring adalah sebuah website yang menawarkan pengiriman makanan via online di restoran favorite mu.</p>
+                    <p>Angkringan adalah sebuah website yang menawarkan pengiriman makanan via online di restoran favorite mu.</p>
 
                     <div class="footContactDetails">
                         <div class="info">
                             <div class="iconDiv"><i class='bx bx-mail-send'></i></div>
-                            <span>Angkring@gmail.com</span>
+                            <span>Angkringan@gmail.com</span>
                         </div>
 
                         <div class="info">
