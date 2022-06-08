959d83c6e3eb5d2ec6904c5d52a216ffa085780f
Rikoairlan <rikoairlan57@gmail.com>
2022-06-08 19:55:18 +0700

    create delete order user

---
+++ delete_orders.php
@@ -0,0 +1,7 @@
+<?php
+include("connection/connect.php");
+error_reporting(0);
+session_start();
+
+mysqli_query($db, "DELETE FROM users_orders WHERE o_id = '" . $_GET['order_del'] . "'");
+header("location:your_orders.php");
