70a5a9010b9a4ea716ae640225c6b611f4446f4d
Rikoairlan <rikoairlan57@gmail.com>
2022-06-09 15:48:49 +0700

    clear

--- connection/connect.php
+++ connection/connect.php
@@ -1,16 +1,12 @@
 <?php
 
-//main connection file for both admin & front end
-$servername = "localhost"; //server
-$username = "root"; //username
-$password = ""; //password
-$dbname = "online_food_order";  //database
+$servername = "localhost"; 
+$username = "root"; 
+$password = ""; 
+$dbname = "online_food_order";  
 
-// Create connection
-$db = mysqli_connect($servername, $username, $password, $dbname); // connecting 
-// Check connection
-if (!$db) {       //checking connection to DB	
+$db = mysqli_connect($servername, $username, $password, $dbname);  
+
+if (!$db) {      	
     die("Connection failed: " . mysqli_connect_error());
 }
-
-?>
