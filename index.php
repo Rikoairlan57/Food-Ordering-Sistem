28c97a06c6c5db02a5469563109a3293646af446
Rikoairlan <rikoairlan57@gmail.com>
2022-06-09 13:38:22 +0700

    create include navbar

--- index.php
+++ index.php
@@ -28,36 +28,9 @@ session_start();
 <body class="home">
 
     <!--header starts-->
-    <header id="header" class="header-scroll top-header headrom headerBg">
-        <!-- .navbar -->
-        <nav class="navbar navbar-dark">
-            <div class="container">
-                <button class="navbar-toggler hidden-lg-up" type="button" data-toggle="collapse" data-target="#mainNavbarCollapse">&#9776;</button>
-                <a class="navbar-brand" href="index.php"> Angkringan </a>
-                <div class="collapse navbar-toggleable-md  float-lg-right" id="mainNavbarCollapse">
-                    <ul class="nav navbar-nav">
-                        <li class="nav-item"> <a class="nav-link active" href="index.php">Home <span class="sr-only">(current)</span></a> </li>
-                        <li class="nav-item"> <a class="nav-link active" href="restaurants.php">Restaurants <span class="sr-only"></span></a> </li>
-                        <?php
-                        if (empty($_SESSION["user_id"])) // if user is not login
-                        {
-                            echo '
-                                <li class="nav-item"><a href="login.php" class="nav-link active">Login</a> </li>
-							    <li class="nav-item"><a href="registration.php" class="nav-link active bgGreen">Signup</a> </li>';
-                        } else {
-                            echo  '<li class="nav-item"><a href="your_orders.php" class="nav-link active">Your Orders</a> </li>';
-                            echo  '<li class="nav-item"><a href="logout.php" class="nav-link active">Logout</a> </li>';
-                        }
+    <?php include("includes/navbar.php") ?>
+    <!-- header end -->
 
-                        ?>
-
-                    </ul>
-
-                </div>
-            </div>
-        </nav>
-        <!-- /.navbar -->
-    </header>
     <!-- banner part starts -->
     <section class="hero bg-image" data-image-src="images/img/banner.jpg">
         <div class="hero-inner">
--- your_orders.php
+++ your_orders.php
@@ -130,36 +130,9 @@ if (empty($_SESSION['user_id'])) {
     <body>
 
         <!--header starts-->
-        <header id="header" class="header-scroll top-header headrom headerBg">
-            <!-- .navbar -->
-            <nav class="navbar navbar-dark">
-                <div class="container">
-                    <button class="navbar-toggler hidden-lg-up" type="button" data-toggle="collapse" data-target="#mainNavbarCollapse">&#9776;</button>
-                    <a class="navbar-brand" href="index.php"> Angkringan </a>
-                    <div class="collapse navbar-toggleable-md  float-lg-right" id="mainNavbarCollapse">
-                        <ul class="nav navbar-nav">
-                            <li class="nav-item"> <a class="nav-link active" href="index.php">Home <span class="sr-only">(current)</span></a> </li>
-                            <li class="nav-item"> <a class="nav-link active" href="restaurants.php">Restaurants <span class="sr-only"></span></a> </li>
+        <?php include("includes/navbar.php") ?>
+        <!-- header end -->
 
-                            <?php
-                            if (empty($_SESSION["user_id"])) {
-                                echo '<li class="nav-item"><a href="login.php" class="nav-link active">Login</a> </li>
-							  <li class="nav-item"><a href="registration.php" class="nav-link active bgGreen">Sign Up</a> </li>';
-                            } else {
-
-
-                                echo  '<li class="nav-item"><a href="your_orders.php" class="nav-link active">My Orders</a> </li>';
-                                echo  '<li class="nav-item"><a href="logout.php" class="nav-link active">Log Out</a> </li>';
-                            }
-
-                            ?>
-
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-            <!-- /.navbar -->
-        </header>
         <div class="page-wrapper">
             <!-- top Links -->
 
