243c92ecb3b78c031c59b2b5f87041a0dda0aad1
Rikoairlan <rikoairlan57@gmail.com>
2022-06-09 12:53:52 +0700

    build include footer

--- checkout.php
+++ checkout.php
@@ -161,74 +161,7 @@ if (empty($_SESSION["user_id"])) {
 
 
         <!-- FOOTER SECTION ----------------------- -->
-        <section class="footerSection">
-            <div class="contentContainer container">
-                <div class="footerIntro">
-                    <div class="footerLogoDiv">
-                        <span class="hotelName">
-                            Angkringan<span>..</span>
-                        </span>
-                    </div>
-                    <p>Angkringan adalah sebuah website yang menawarkan pengiriman makanan via online di restoran favorite mu.</p>
-
-                    <div class="footContactDetails">
-                        <div class="info">
-                            <div class="iconDiv"><i class='bx bx-mail-send'></i></div>
-                            <span>Angkringan@gmail.com</span>
-                        </div>
-
-                        <div class="info">
-                            <div class="iconDiv"><i class='bx bxs-phone-outgoing'></i></div>
-                            <span>+62 8990761528</span>
-                        </div>
-
-                        <div class="info">
-                            <div class="iconDiv"><i class='bx bx-current-location'></i></div>
-                            <span>jl.blok duku, jakarta timur</span>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="linksDiv">
-                    <div class="footersectionTitle">
-                        <h5>Mitra</h5>
-                    </div>
-                    <ul>
-                        <span>Gojek</span>
-                        <span>Grab</span>
-                        <span>Paypal</span>
-                        <span>Affiliation</span>
-                        <span>FAQs</span>
-                    </ul>
-                </div>
-
-                <div class="linksDiv">
-                    <div class="footersectionTitle">
-                        <h5>OUR SERVICES</h5>
-                    </div>
-                    <ul>
-                        <span>Online Order Food</span>
-                        <span>Free Ongkir</span>
-                        <span>Cash on Delivery</span>
-                        <span>Wishlist</span>
-                        <span>Discount</span>
-                    </ul>
-                </div>
-
-                <div class="linksDiv footerForm">
-                    <div class="footersectionTitle">
-                        <h5> OUR NEWSLETTER</h5>
-                    </div>
-
-                    <form action="">
-                        <label> Subscribe To Our Newsletter...</label>
-                        <input type="text" placeholder="Name" required>
-                        <input type="email" placeholder="Email" required>
-                        <button type="submit">Submit</button>
-                    </form>
-                </div>
-            </div>
-        </section>
+        <?php include("includes/footer.php"); ?>
         <!-- FOOTER SECTION END----------------- -->
         </div>
         <!-- end:page wrapper -->
