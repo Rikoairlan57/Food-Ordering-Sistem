a13c75a226eb57f919bafb558a31ca5ea27f5e05
Rikoairlan <rikoairlan57@gmail.com>
2022-09-04 19:52:45 +0700

    add db

---
+++ online_food_order.sql
@@ -0,0 +1,341 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 04 Sep 2022 pada 14.47
+-- Versi server: 10.4.24-MariaDB
+-- Versi PHP: 7.4.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `online_food_order`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `admin`
+--
+
+CREATE TABLE `admin` (
+  `adm_id` int(222) NOT NULL,
+  `username` varchar(222) NOT NULL,
+  `password` varchar(222) NOT NULL,
+  `email` varchar(222) NOT NULL,
+  `code` varchar(222) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `admin`
+--
+
+INSERT INTO `admin` (`adm_id`, `username`, `password`, `email`, `code`, `date`) VALUES
+(9, 'admin1234', '0192023a7bbd73250516f069df18b500', 'admin@gmail.com', 'QFE6ZM', '2022-04-08 08:24:52'),
+(10, 'rikoairlan123', 'a15f31d3578991b0d7734fc6179068e5', 'rikoairlan123@gmail.com', 'QMZR92', '2022-04-08 08:39:32');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `admin_codes`
+--
+
+CREATE TABLE `admin_codes` (
+  `id` int(222) NOT NULL,
+  `codes` varchar(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `admin_codes`
+--
+
+INSERT INTO `admin_codes` (`id`, `codes`) VALUES
+(1, 'QX5ZMN'),
+(2, 'QFE6ZM'),
+(3, 'QMZR92'),
+(4, 'QPGIOV'),
+(5, 'QSTE52'),
+(6, 'QMTZ2J');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `dishes`
+--
+
+CREATE TABLE `dishes` (
+  `d_id` int(222) NOT NULL,
+  `rs_id` int(222) NOT NULL,
+  `title` varchar(222) NOT NULL,
+  `slogan` varchar(222) NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `img` varchar(222) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `dishes`
+--
+
+INSERT INTO `dishes` (`d_id`, `rs_id`, `title`, `slogan`, `price`, `img`) VALUES
+(15, 51, 'Paket Promo 1', '2 ayam + nasi + free minuman pink lava', '45000.00', '629f1cd8890da.jpg'),
+(16, 49, 'Double Cheeseburger', 'in deliciuse double chesee burger is goood', '70000.00', '628dbe10cde2b.jpg'),
+(17, 53, 'Chicken Katsu', 'ayam tepung dengan bumbu special', '50000.00', '628db1d3aac1e.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `remark`
+--
+
+CREATE TABLE `remark` (
+  `id` int(11) NOT NULL,
+  `frm_id` int(11) NOT NULL,
+  `status` varchar(255) NOT NULL,
+  `remark` mediumtext NOT NULL,
+  `remarkDate` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `remark`
+--
+
+INSERT INTO `remark` (`id`, `frm_id`, `status`, `remark`, `remarkDate`) VALUES
+(71, 41, 'in process', 'in proses', '2022-04-08 08:40:37'),
+(72, 44, 'in process', 'sedang di masak', '2022-05-25 04:21:29'),
+(73, 44, 'closed', 'sedang di antar', '2022-05-29 12:50:12'),
+(74, 44, 'in process', 'sedang di masak', '2022-06-04 00:43:02'),
+(75, 44, 'in process', 'sedang di buat', '2022-06-07 11:48:43'),
+(76, 49, 'in process', 'sedang di masak', '2022-06-08 11:44:59'),
+(77, 49, 'in process', 'process bre', '2022-06-13 08:45:15'),
+(78, 49, 'closed', 'silahkan menikmati\r\n', '2022-06-13 12:02:40'),
+(79, 50, 'closed', 'segera datang', '2022-06-13 12:12:59'),
+(80, 50, 'in process', 'sdad', '2022-06-13 12:14:13');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `restaurant`
+--
+
+CREATE TABLE `restaurant` (
+  `rs_id` int(222) NOT NULL,
+  `c_id` int(222) NOT NULL,
+  `title` varchar(222) NOT NULL,
+  `email` varchar(222) NOT NULL,
+  `phone` varchar(222) NOT NULL,
+  `url` varchar(222) NOT NULL,
+  `o_hr` varchar(222) NOT NULL,
+  `c_hr` varchar(222) NOT NULL,
+  `o_days` varchar(222) NOT NULL,
+  `address` text NOT NULL,
+  `image` text NOT NULL,
+  `date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `restaurant`
+--
+
+INSERT INTO `restaurant` (`rs_id`, `c_id`, `title`, `email`, `phone`, `url`, `o_hr`, `c_hr`, `o_days`, `address`, `image`, `date`) VALUES
+(48, 14, 'PHD Pizza', 'phdpizza@gmail.com', '253243', 'phdpizza.com', '24hour', '24hour', 'every-day', '  phd pizza delivery open 24jam non stop  ', '629f38f76bff0.jpg', '2022-06-07 11:39:35'),
+(49, 12, 'Burger King', 'burgerking@gmail.com', '7087070977', 'burgerking.com', '7am', '8pm', '24hr-x7', ' burger king is not krebipetie ', '628db64583de8.png', '2022-05-25 04:53:25'),
+(50, 13, 'Solaria', 'solaria@gmail.com', '088564567', 'Solaria.com', '9am', '8pm', '24hr-x7', '  solaria menyajikan makanan keluarga   ', '628dbf8311016.jpg', '2022-05-25 05:32:51'),
+(51, 13, 'Richeese factory', 'richeesefactory@gmail.com', '3454345654', 'richeesefactory.com', '9am', '8pm', '24hr-x7', 'fire chicken ', '628dda61446a8.jpg', '2022-05-25 07:27:29'),
+(52, 13, 'KFC', 'kfc@gmail.com', '2345434567', 'kfc.com', '8am', '8pm', '24hr-x7', 'promo ayam beli 2 free 1', '628dd888d4ed5.jpg', '2022-05-25 07:19:36'),
+(53, 8, 'Hokben1', 'Hokben@gmail.com', '4512545784', 'Hokben.com', '24hour', '24hour', 'every-day', '  Japanese food   ', '62a822b395fad.jpg', '2022-06-14 05:54:59');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `res_category`
+--
+
+CREATE TABLE `res_category` (
+  `c_id` int(222) NOT NULL,
+  `c_name` varchar(222) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `res_category`
+--
+
+INSERT INTO `res_category` (`c_id`, `c_name`, `date`) VALUES
+(8, 'JapaneseFood', '2022-05-25 07:35:48'),
+(11, 'Steak', '2022-05-24 12:30:09'),
+(12, 'Burger', '2022-05-25 04:52:42'),
+(13, 'FamillyFood', '2022-05-25 05:31:36'),
+(17, 'Pizza', '2022-06-13 11:22:21');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `users`
+--
+
+CREATE TABLE `users` (
+  `u_id` int(222) NOT NULL,
+  `username` varchar(222) NOT NULL,
+  `f_name` varchar(222) NOT NULL,
+  `l_name` varchar(222) NOT NULL,
+  `email` varchar(222) NOT NULL,
+  `phone` varchar(222) NOT NULL,
+  `password` varchar(222) NOT NULL,
+  `address` text NOT NULL,
+  `status` int(222) NOT NULL DEFAULT 1,
+  `date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `users`
+--
+
+INSERT INTO `users` (`u_id`, `username`, `f_name`, `l_name`, `email`, `phone`, `password`, `address`, `status`, `date`) VALUES
+(33, 'rikoairlan', 'Riko', 'Airlan', 'rikoairlan@gmail.com', '08990761528', 'a15f31d3578991b0d7734fc6179068e5', 'jl.blok duku', 1, '2022-04-08 08:35:14'),
+(36, 'test1', 'riko', 'airlan', 'rikoairlan1111@gmail.com', '08990761525', 'a15f31d3578991b0d7734fc6179068e5', 'jl.blok duku\r\nrt004 rw 010', 1, '2022-06-14 04:17:26');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `users_orders`
+--
+
+CREATE TABLE `users_orders` (
+  `o_id` int(222) NOT NULL,
+  `u_id` int(222) NOT NULL,
+  `title` varchar(222) NOT NULL,
+  `quantity` int(222) NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `status` varchar(222) DEFAULT NULL,
+  `date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `users_orders`
+--
+
+INSERT INTO `users_orders` (`o_id`, `u_id`, `title`, `quantity`, `price`, `status`, `date`) VALUES
+(49, 33, 'Paket Promo 1', 2, '45000.00', 'closed', '2022-06-13 12:02:40'),
+(50, 33, 'Fire Wing', 2, '30000.00', 'in process', '2022-06-13 12:14:13'),
+(54, 33, 'Chicken Katsu', 1, '50000.00', NULL, '2022-06-14 04:19:35'),
+(55, 33, 'Chicken Katsu', 1, '50000.00', NULL, '2022-06-14 04:19:52'),
+(56, 33, 'Chicken Katsu', 1, '50000.00', NULL, '2022-06-14 04:20:07');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`adm_id`);
+
+--
+-- Indeks untuk tabel `admin_codes`
+--
+ALTER TABLE `admin_codes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `dishes`
+--
+ALTER TABLE `dishes`
+  ADD PRIMARY KEY (`d_id`);
+
+--
+-- Indeks untuk tabel `remark`
+--
+ALTER TABLE `remark`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `restaurant`
+--
+ALTER TABLE `restaurant`
+  ADD PRIMARY KEY (`rs_id`);
+
+--
+-- Indeks untuk tabel `res_category`
+--
+ALTER TABLE `res_category`
+  ADD PRIMARY KEY (`c_id`);
+
+--
+-- Indeks untuk tabel `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`u_id`);
+
+--
+-- Indeks untuk tabel `users_orders`
+--
+ALTER TABLE `users_orders`
+  ADD PRIMARY KEY (`o_id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `adm_id` int(222) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT untuk tabel `admin_codes`
+--
+ALTER TABLE `admin_codes`
+  MODIFY `id` int(222) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT untuk tabel `dishes`
+--
+ALTER TABLE `dishes`
+  MODIFY `d_id` int(222) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+
+--
+-- AUTO_INCREMENT untuk tabel `remark`
+--
+ALTER TABLE `remark`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=81;
+
+--
+-- AUTO_INCREMENT untuk tabel `restaurant`
+--
+ALTER TABLE `restaurant`
+  MODIFY `rs_id` int(222) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=56;
+
+--
+-- AUTO_INCREMENT untuk tabel `res_category`
+--
+ALTER TABLE `res_category`
+  MODIFY `c_id` int(222) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- AUTO_INCREMENT untuk tabel `users`
+--
+ALTER TABLE `users`
+  MODIFY `u_id` int(222) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+
+--
+-- AUTO_INCREMENT untuk tabel `users_orders`
+--
+ALTER TABLE `users_orders`
+  MODIFY `o_id` int(222) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=58;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
